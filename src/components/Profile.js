02ec36d5f853c614b315e47a8d8ceb9b52fbbfa5
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-05-21 18:09:13 -0400

    create Profile component

---
+++ src/components/Profile.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export class Profile extends Component {
+
+  state = {
+    
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Profile
