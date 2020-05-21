d49db0578c0510fa11d59174ea0bb4d017da7abf
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-05-21 18:55:16 -0400

    add Profile component logic

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,18 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class Profile extends Component {
+const Profile = props => {
 
-  state = {
-    
-  }
-
-  render() {
+  const {firstName, lastName, phoneNumber, creditCard, address1, address2, city, state, zipcode} = props.currentUser
+  
     return (
       <div>
-        
+        <h1>Profile Page</h1>
+        <h2>{firstName} {lastName}</h2>
+        <h3>Address: </h3>
+        {address1}
+        {address2}
+        {city}
+        {state}
+        {zipcode}
+        <h3>Phone number: </h3>
+        {phoneNumber}
+
+        <h3>Payment Information</h3>
+        <p>Credit Card: {creditCard}</p>
+
+        <button>Delete Account</button>
+        <button>Edit Profile</button>
       </div>
     )
-  }
 }
 
 export default Profile
