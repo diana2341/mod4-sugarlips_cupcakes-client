00f71f10bb61483e2594a44e37558e2da52d66d6
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-05-22 13:55:10 -0400

    update components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,51 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import MainContainer from './components/MainContainer';
 import HomePage from './components/HomePage'
 import Profile from './components/Profile'
-import Form from './components/Form'
+// import Form from './components/Form'
 import Signup from './components/Signup'
 import EditProfile from './components/EditProfile'
 import CupcakeContainer from './components/CupcakeContainer'
-import Navbar from './components/Navbar'
-import { Route, Link, Switch, Redirect } from 'react-router-dom';
-import MainContainer from './components/MainContainer';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-     <Navbar/>
         <Switch>
-    <Route path="/home" render={(routerProps) =><HomePage {...routerProps}/>}/>
-    <Route path="/profile" render={(routerProps) => <Profile 
-            currentUser={this.state.currentUser} />}/>
-    <Route path="/sign-up" render={(routerProps) =><Signup 
+          <Route 
+            path="/home" 
+            render={(routerProps) => 
+            <HomePage {...routerProps}/>}
+            />
+          <Route 
+            path="/profile" 
+            render={(routerProps) => <Profile 
+            currentUser={this.state.currentUser} />} 
+            />
+          <Route 
+            path="/sign-up" 
+            render={(routerProps) => <Signup 
             form={this.state.form}
-            handleChange={this.handleChange} />}/>
-    <Route path="/edit-profile" render={(routerProps) =><EditProfile 
+            handleChange={this.handleChange} />} 
+            />
+          <Route 
+            path="/edit-profile" 
+            render={(routerProps) => <EditProfile 
             form={this.state.form}
-            handleChange={this.handleChange} />}/>
-    <Route path="/form" render={(routerProps) =>< Form 
+            handleChange={this.handleChange} />} 
+            />
+          {/* <Route 
+            path="/form" 
+            render={(routerProps) => <Form 
             form={this.state.form}
-            handleChange={this.handleChange} />}/>
-            {/* form for? */}
-    <Route path="/cupcakes" render={()=><CupcakeContainer/>}/>
-       
+            handleChange={this.handleChange} />}
+            /> */}
+          <Route 
+            path="/cupcakes" 
+            render={()=><CupcakeContainer/>}/>
           </Switch>
         <MainContainer />
       </div>
@@ -40,4 +54,3 @@ class App extends Component {
 }
 
 export default App;
-  
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Search from './Search'
 import Sort from './Sort'
 
