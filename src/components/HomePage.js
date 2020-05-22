95e541f2407b2c7acbea455e9f45e9a4a64f3dd8
diana2341 <dianaponce2341@gmail.com>
2020-05-22 15:37:06 -0400

    front page button

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import Login from './Login'
 
 class HomePage extends React.Component {
+handClick=()=>{
+this.props.history.push('/cupcakes') 
 
+      }
   render() {
+      
     return( 
       <div>
         <h3>{this.props.sample}</h3>
         <h1>Make yourself at home with gourmet cupcake.</h1>
-        <Login />
+        <button onClick={this.handClick}>enter</button>
+        
       </div>
     )
   }
