171e18591957237895a0c732c6bfcd06f6cabadb
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-05-22 15:40:52 -0400

    changes

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,8 +6,7 @@ class HomePage extends React.Component {
   render() {
     return( 
       <div>
-        <h3>{this.props.sample}</h3>
-        <h1>Make yourself at home with gourmet cupcake.</h1>
+        <h1>Make yourself at home with gourmet cupcake</h1>
         <Login />
       </div>
     )
