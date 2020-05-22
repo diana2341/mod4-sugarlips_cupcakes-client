37835036dc4d32758116106747c3da3c8a982b18
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-05-22 14:05:48 -0400

    reformat navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,22 +6,22 @@ import Sort from './Sort'
 const Navbar = () => {
   return (
     <div className="container">
-        <header>
-      <div className="logo">
-        <a ><Link to="/cupcakes">Sugar Lipss</Link> </a>
-      </div>
-      <nav>
-        <ul>
-          <li><Link to="/home">Main page</Link></li>
-          <li><Link to="/sign-up">Sign up</Link></li>
-          <li><Link to="/profile">Account</Link></li>
-          {/* <li><Link to="/">?</Link></li> */}
-          {/* <li><Link to="/">?</Link></li> */}
-        </ul>
-      </nav>
-    </header>
-      <Search />
-      <Sort />
+      <header>
+        <div className="logo">
+          <a ><Link to="/cupcakes">Sugar Lipss</Link> </a>
+        </div>
+        <Search />
+        <Sort />
+        <nav>
+          <ul>
+            <li><Link to="/home">Main page</Link></li>
+            <li><Link to="/sign-up">Sign up</Link></li>
+            <li><Link to="/profile">Account</Link></li>
+            {/* <li><Link to="/">?</Link></li> */}
+            {/* <li><Link to="/">?</Link></li> */}
+          </ul>
+        </nav>
+      </header>
     </div>
   )
 }
