eb332c17229b8b3c4003d5510f7870d08afc7f3c
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-05-22 11:15:34 -0400

    create login component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,50 +1,18 @@
 import React from 'react'
+import Login from './Login'
 import CupcakeContainer from './CupcakeContainer'
 
 
 class HomePage extends React.Component {
-    state={
-        username:'',
-        password:''
-    }
-handleChange=(event)=>{
-    const{name,value}=event.target
-    this.setState({[name]:value})
-}
- 
-    render() {
-        console.log(this.state.username)
-        console.log(this.state.password)
-      return( 
-        <div>
+  render() {
+    return( 
+      <div>
         <h1>Make yourself at home with gourmet cupcake.</h1>
-{/* make login component */}
-    <div className="login-box">
-	<h2>Login</h2>
-
-	<form >
-		<div className="user-box">
-			<input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
-			<label>Username</label>
-		</div>
-
-		<div className="user-box">
-			<input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
-			<label >Password</label>
-		</div>
-        
-		<a href="">
-			<span></span>
-			<span></span>
-			<span></span>
-			<span></span>
-			Enter
-		</a>
-	</form>
-    </div>
-      <CupcakeContainer/>
-        </div>
-      )
-    }
+        <Login />
+        <CupcakeContainer />
+      </div>
+    )
   }
+}
+
 export default HomePage
---
+++ src/components/Login.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+
+export default class Login extends Component {
+
+  state = {
+    username:'',
+    password:''
+  }
+
+  handleChange = event => {
+    const {name, value} = event.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  render() {
+    console.log(this.state.username)
+    console.log(this.state.password)
+
+    return (
+      <div className="login-box">
+        <h2>Login</h2>
+      
+        <form >
+          <div className="user-box">
+            <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
+            <label>Username</label>
+          </div>
+      
+          <div className="user-box">
+            <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
+            <label >Password</label>
+          </div>
+              
+          <a href="">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Enter
+          </a>
+        </form>
+      </div>
+    )
+  }
+}
+
