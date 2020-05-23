5b3949da30795be61228b3bf7dba51814ae15119
diana2341 <dianaponce2341@gmail.com>
2020-05-23 13:39:50 -0400

    profile pg

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,34 +6,39 @@ const Profile = props => {
   
     return (
       <div>
-        
+         <h1>Profile Page</h1>
         <div class="card-profile">
-  <div class="card-borders">
-    <div class="border-top"></div>
-    <div class="border-right"></div>
-    <div class="border-bottom"></div>
-    <div class="border-left"></div>
+  <div className="card-borders">
+    <div className="border-top"></div>
+    <div className="border-right"></div>
+    <div className="border-bottom"></div>
+    <div className="border-left"></div>
   </div>
-  <div class="card-content">
-  <div class="profileImg"> <img src="https://cdn4.vectorstock.com/i/1000x1000/76/43/muffin-with-kawaii-face-design-vector-10657643.jpg" style={{"width":"110px"}}/>
+  <div className="card-content">
+  <div className="profileImg"> <img src="https://cdn4.vectorstock.com/i/1000x1000/76/43/muffin-with-kawaii-face-design-vector-10657643.jpg" style={{"width":"110px"}}/>
     
 </div>
-    {/* <div class="username">alphardex</div> */}
-        <h1>Profile Page</h1>
-        <h2 class="username">{firstName} {lastName}</h2>
+       
+        <h2 className="username">{firstName} {lastName}</h2>
+        <div className="profile-information">
         <h3>Address</h3>
-        {address1} <br />
-        {address2} <br />
-        {city}, {state} {zipcode}
-
+       
+       <p className="info-p" ><strong >Address 1:</strong> {address1} </p><br/>
+       <p className="info-p"> <strong >Address 2:</strong> {address2} </p> <br/>
+       <p className="info-p"> <strong >city:</strong> {city} </p><br/>
+       <p className="info-p"> <strong >state:</strong>{state} </p><br/>
+        <p className="info-p"><strong >zipcode:</strong>{zipcode} </p><br/>
+     
         <h3>Contact Information</h3>
         {phoneNumber}
 
         <h3>Payment Information</h3>
         <p>Credit Card: {creditCard}</p>
-
-        <button>Delete Account</button>
-        <button>Edit Profile</button>
+        </div>
+      <div className="profile-buttons">
+        <button className="profile-button-design">Delete Account</button><br/>
+        <button className="profile-button-design">Edit Profile</button>
+      </div>
         {/* </div> */}
        </div>
       </div>
