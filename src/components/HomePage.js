69d2a11306995b090c252e3126e4c683d9a7153e
diana2341 <dianaponce2341@gmail.com>
2020-05-25 22:14:16 -0400

    entrance almost done

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -13,7 +13,7 @@ class HomePage extends React.Component {
     let cp1=require("../cupcake-img/mustache.png")
     let cp2=require("../cupcake-img/bluebuerry.png")
     let cp3=require("../cupcake-img/goth.png")
-    let cp4=require("../cupcake-img/heartstrawberry.png")
+    // let cp4=require("../cupcake-img/heartstrawberry.png")
     let cp5=require("../cupcake-img/lemon.png")
     let cp6=require("../cupcake-img/xoxo.png")
     let cp7=require("../cupcake-img/sprinkles.png")
@@ -26,32 +26,35 @@ class HomePage extends React.Component {
     return( 
       <div>
           <div className="center-entrance">
-             <h1 className="text-c">Make yourself at home with gourmet cupcake</h1>
+             <h1 className="text-c">Some only dream of cupcakes others make it happen</h1>
               <button onClick={this.handClick}>enter</button>
 
           </div>
+          <div className="stache">
+                  <img className="mus" src={cp1} alt="cupcake"/>
+        </div> 
                
         <div className="rotate">
-                  <img className="mustache" src={cp1} alt="cupcake"/>
+                  <img className="mustache" src={cp9} alt="cupcake"/>
         </div>  
 
-        <div className="rotate2">
-                  <img className="mustache" src={cp2} alt="cupcake"/>
-        </div> 
+        {/* <div className="rotate2">
+                  <img className="blueberry" src={cp2} alt="cupcake"/>
+        </div>  */}
         <div className="rotate3">
                   <img className="goth" src={cp3} alt="cupcake"/>
         </div> 
-        {/* <div className="rotate5">
-                  <img className="mustache" src={cp5} alt="cupcake"/>
+        <div className="rotate4">
+                  <img className="goth2" src={cp7} alt="cupcake"/>
+        </div> 
+        <div className="rotate5">
+                  <img className="lemon" src={cp5} alt="cupcake"/>
         </div> 
         <div className="rotate6">
-                  <img className="mustache" src={cp6} alt="cupcake"/>
+                  <img className="sprinlke-front-page" src={cp6} alt="cupcake"/>
         </div> 
-        <div className="rotate7">
+        {/* <div className="rotate7">
                   <img className="mustache" src={cp7} alt="cupcake"/>
-        </div> 
-        <div className="rotate8">
-                  <img className="mustache" src={cp9} alt="cupcake"/>
         </div>    */}
 
        
